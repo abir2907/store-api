b3a8f4d83b39df1bc2f14a20c7fe57a22e4bc272
Abir Chodha <abirchodha1@gmail.com>
2025-03-02 19:42:02 +0530

    Enable searching by part of name If we did queryObject.name = name then the name from req.query would have to exaclty match the name of some product, but $regex:name allows us to serach for pattern, eg. name of 'ab' will match with table and we will get table product. $options:'i' is for case insensitive

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -17,6 +17,10 @@ const getAllProducts = async (req, res) => {
     queryObject.company = company;
   }
 
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObject);
   const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
